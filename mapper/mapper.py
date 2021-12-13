d9db5d1d71924d1c36cf499323b483e6297c555a
Jasper Horn <jasperhorn@gmail.com>
2021-12-13 20:57:54 +0100

    Turn pixels on one by one and map them

--- mapper/mapper.py
+++ mapper/mapper.py
@@ -1,6 +1,13 @@
+
+import time
+
 import pygame
 import pygame.camera
-import time
+
+import neopixel
+import board
+
+highlightColor = (255, 128, 0);
 
 pygame.camera.init()
 cams = pygame.camera.list_cameras() 
@@ -11,31 +18,35 @@ cam.start()
 # First image often contains a weird brightness tear, so we discard it
 cam.get_image()
 
-img = cam.get_image()
+leds = neopixel.NeoPixel(board.D18, 50, pixel_order=neopixel.RGB)
 
-pygame.image.save(img,"photo.jpg")
+for i in range(50):
+    leds[i] = (255, 255, 255)
 
-brightest = 0
-brightestSpot = None
+    img = cam.get_image()
 
-for x in range(img.get_width()):
+    pygame.image.save(img,"output/led-" + str(i) + ".jpg")
+
+    brightest = 0
+    brightestSpot = None
+
+    for x in range(img.get_width()):
+        for y in range(img.get_height()):
+            color = img.get_at((x, y))
+            brightness = color.r + color.g + color.b
+
+            if brightness > brightest:
+                brightest = brightness
+                brightestSpot = (x, y)
+
+    leds[i] = (0, 0, 0)
+
+    for x in range(img.get_width()):
+        img.set_at((x, brightestSpot[1]), highlightColor)
+
     for y in range(img.get_height()):
-        color = img.get_at((x, y))
-        brightness = color.r + color.g + color.b
+        img.set_at((brightestSpot[0], y), highlightColor)
 
-        if brightness > brightest:
-            brightest = brightness
-            brightestSpot = (x, y)
+    pygame.image.save(img,"output/led-" + str(i) + "-highlighted.jpg")
 
-print(brightestSpot)
-print(brightness)
-
-highlightColor = (255, 128, 0);
-
-for x in range(img.get_width()):
-    img.set_at((x, brightestSpot[1]), highlightColor)
-
-for y in range(img.get_height()):
-    img.set_at((brightestSpot[0], y), highlightColor)
-
-pygame.image.save(img,"photo-highlighted.jpg")
+    print("Led " + str(i) + " mapped")
