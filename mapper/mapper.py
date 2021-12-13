8cdb3b76555b7467a8a190e34e7e3df0e4666ef4
Jasper Horn <jasperhorn@gmail.com>
2021-12-13 18:31:19 +0100

    Detect the brightest pixel in the photo

--- mapper/mapper.py
+++ mapper/mapper.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame.camera
+import time
 
 pygame.camera.init()
 cams = pygame.camera.list_cameras() 
@@ -7,5 +8,34 @@ cam = pygame.camera.Camera(cams[0], (1280,720))
 
 cam.start()
 
+# First image often contains a weird brightness tear, so we discard it
+cam.get_image()
+
 img = cam.get_image()
+
 pygame.image.save(img,"photo.jpg")
+
+brightest = 0
+brightestSpot = None
+
+for x in range(img.get_width()):
+    for y in range(img.get_height()):
+        color = img.get_at((x, y))
+        brightness = color.r + color.g + color.b
+
+        if brightness > brightest:
+            brightest = brightness
+            brightestSpot = (x, y)
+
+print(brightestSpot)
+print(brightness)
+
+highlightColor = (255, 128, 0);
+
+for x in range(img.get_width()):
+    img.set_at((x, brightestSpot[1]), highlightColor)
+
+for y in range(img.get_height()):
+    img.set_at((brightestSpot[0], y), highlightColor)
+
+pygame.image.save(img,"photo-highlighted.jpg")
