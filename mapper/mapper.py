bf496acdff07fdeeac86f9e49d0ba4d38eb92fd9
Jasper Horn <jasperhorn@gmail.com>
2021-12-13 22:57:42 +0100

    Different strategy for defining brightest spot
    Previously: try to get the pixels with most color
    Now: Just take every pixel where each color is above 220
    
    Previously: Turn on LED at its fullest
    Now: Only turn on the LED a bit to prevent reflections creating noise

--- mapper/mapper.py
+++ mapper/mapper.py
@@ -16,7 +16,7 @@ cam = pygame.camera.Camera(cams[0], (1280,720))
 leds = neopixel.NeoPixel(board.D18, 50, pixel_order=neopixel.RGB)
 
 for i in range(50):
-    leds[i] = (255, 255, 255)
+    leds[i] = (50, 50, 50)
 
     # Toggling on and off camera each time because otherwise I appear to get old images 
     cam.start()
@@ -27,25 +27,29 @@ for i in range(50):
 
     pygame.image.save(img,"output/led-" + str(i) + ".jpg")
 
-    brightest = 0
-    brightestSpot = None
+    brightestSpot = (0, 0)
+    brightestWeight = 0
 
     for x in range(img.get_width()):
         for y in range(img.get_height()):
             color = img.get_at((x, y))
             brightness = color.r + color.g + color.b
 
-            if brightness > brightest:
-                brightest = brightness
-                brightestSpot = (x, y)
+            if color.r > 220 and color.g > 220 and color.b > 220:
+                brightestSpot = ((brightestSpot[0] * brightestWeight + x) / (brightestWeight + 1),
+                                 (brightestSpot[1] * brightestWeight + y) / (brightestWeight + 1))
+
+                brightestWeight += 1
 
     leds[i] = (0, 0, 0)
 
+    roundedBrightestSpot = (round(brightestSpot[0]), round(brightestSpot[1]))
+
     for x in range(img.get_width()):
-        img.set_at((x, brightestSpot[1]), highlightColor)
+        img.set_at((x, roundedBrightestSpot[1]), highlightColor)
 
     for y in range(img.get_height()):
-        img.set_at((brightestSpot[0], y), highlightColor)
+        img.set_at((roundedBrightestSpot[0], y), highlightColor)
 
     pygame.image.save(img,"output/led-" + str(i) + "-highlighted.jpg")
 
